69b42052eeff324d32c955b2fc7076d98f8f64bf
Filippos Vasilakis <vasilakisfil@gmail.com>
2021-11-08 13:30:54 +0100

    Include Joplin to apps without title bars

--- .config/devilspie2/default.lua
+++ .config/devilspie2/default.lua
@@ -11,3 +11,8 @@ if (string.find(get_application_name(),"Slack")~=nil) then
     maximize()
     undecorate_window()
 end
+
+if (string.find(get_application_name(),"Joplin")~=nil) then
+    maximize()
+    undecorate_window()
+end
